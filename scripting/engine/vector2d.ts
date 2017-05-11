eb48af8da6a7068660c97adefcb497fa461d8938
Sebastiaan Vandekerkhove <svandekerkhove@usgpeople.be>
2017-05-11 15:53:10 +0200

    Creation of engine components and initial object

---
+++ scripting/engine/vector2d.ts
@@ -0,0 +1,8 @@
+// 2d vector class (coordinate system, positioning)
+
+export class vector2d{
+    // X coordinate relative to origin
+    X:number;
+    // Y coordinate relative to origin
+    Y:number;
+}
