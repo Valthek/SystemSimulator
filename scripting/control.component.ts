a9c49d307eb3c2c3d3694052056800ee02851f21
Sebastiaan Vandekerkhove <svandekerkhove@usgpeople.be>
2017-05-12 09:20:03 +0200

    removed unneeded files

--- scripting/control.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from 'angular2/core';
- 
-@Component({
-  selector: 'time',
-  template: "<p>Change the time</p>"
-})
-export class ControlComponent { }
--- scripting/course.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from 'angular2/core';
- 
-@Component({
-  selector: 'course',
-  template: "<p>Plot your course</p>"
-})
-export class CourseComponent { }
