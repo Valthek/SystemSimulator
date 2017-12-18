226980f329a33b1f93290bd2bf91637b9684e980
Sebastiaan Vandekerkhove <svandekerkhove@usgpeople.be>
2017-12-18 14:07:12 +0100

    Linted LoadObjects

--- src/app/system/loadObjects.ts
+++ src/app/system/loadObjects.ts
@@ -1,78 +1,75 @@
-import { planet } from "./objects/planet";
-import { moon } from "./objects/moon";
-import { cObject } from "./objects/cObject";
+import { planet } from './objects/planet';
+import { moon } from './objects/moon';
+import { cObject } from './objects/cObject';
 import { Directive } from '@angular/core';
 import { vector2d } from './engine/vector2d';
 
 export class loadObjects {
 
-  static loadPlanets(source:string): planet[] {
-    let planets: planet[] = [];
-    let planetNumber: number = 0;
+  static loadPlanets(source: string): planet[] {
+    const planets: planet[] = [];
+    let planetNumber = 0;
 
     $.ajax({
-      type: "GET",
-      url: "./data/" + source,
-      dataType: "xml",
+      type: 'GET',
+      url: './data/' + source,
+      dataType: 'xml',
       success: function (data) {
-        $(data).find("planet").each(function (index) {
+        $(data).find('planet').each(function (index) {
           let moonNumber = 0;
-          let name = $(this).find("name").text();
-          let distanceToOrigin = parseFloat($(this).find("distance").text());
-          let radialVelocity = parseFloat($(this).find("radialVelocity").text());
-          let initialAngle = parseFloat($(this).find("startAngle").text());
-          let planetColor = $(this).find("planetColor").text();
-          let actualDiameter = parseFloat($(this).find("realDiameter").text());
-          let moons: moon[] = [];
-          $(this).find("moon").each(function () {
-            let moonName = $(this).find("moonName").text();
-            let moonSpeed = parseFloat($(this).find("moonSpeed").text());
-            let moonStartAngle = parseFloat($(this).find("moonStartAngle").text());
-            let moonColor = $(this).find("moonColor").text();
-            let moonActualDiameter = parseFloat($(this).find("realMoonDiameter").text());
-            let moonOrbitRadius = parseFloat($(this).find("moonOrbitRadius").text());
-            let m: moon = new moon(moonNumber, moonName, moonActualDiameter, moonColor, moonOrbitRadius, moonSpeed, moonStartAngle);
+          const name = $(this).find('name').text();
+          const distanceToOrigin = parseFloat($(this).find('distance').text());
+          const radialVelocity = parseFloat($(this).find('radialVelocity').text());
+          const initialAngle = parseFloat($(this).find('startAngle').text());
+          const planetColor = $(this).find('planetColor').text();
+          const actualDiameter = parseFloat($(this).find('realDiameter').text());
+          const moons: moon[] = [];
+          $(this).find('moon').each(function () {
+            const moonName = $(this).find('moonName').text();
+            const moonSpeed = parseFloat($(this).find('moonSpeed').text());
+            const moonStartAngle = parseFloat($(this).find('moonStartAngle').text());
+            const moonColor = $(this).find('moonColor').text();
+            const moonActualDiameter = parseFloat($(this).find('realMoonDiameter').text());
+            const moonOrbitRadius = parseFloat($(this).find('moonOrbitRadius').text());
+            const m: moon = new moon(moonNumber, moonName, moonActualDiameter, moonColor, moonOrbitRadius, moonSpeed, moonStartAngle);
             moons.push(m);
-            //console.log(m);
             moonNumber++;
           });
-          let p: planet = new planet(planetNumber,name,planetColor, actualDiameter,moons, distanceToOrigin, radialVelocity,initialAngle);
+          const p: planet = new planet(planetNumber, name, planetColor, actualDiameter, moons, distanceToOrigin, radialVelocity, initialAngle);
           planets.push(p);
-          //console.log(p);
           planetNumber++;
         });
       },
       error: function () {
-        console.log("An error loading the xml file has occured");
+        console.log('An error loading the xml file has occured');
       }
     });
     return planets;
   }
 
-  static loadCObjects(source:string): cObject[] {
-    let cObjects: cObject[] = [];
-    let cObjectNumber:number = 0;
+  static loadCObjects(source: string): cObject[] {
+    const cObjects: cObject[] = [];
+    let cObjectNumber = 0;
     $.ajax({
-      type: "GET",
-      url: "/data/" + source,
-      dataType: "xml",
+      type: 'GET',
+      url: '/data/' + source,
+      dataType: 'xml',
       success: function (data) {
-        $(data).find("cObject").each(function (index) {
-          let name = $(this).find("name").text();
-          let distanceToOrigin = parseFloat($(this).find("distance").text());
-          let initialAngle = parseFloat($(this).find("startAngle").text());
-          let radialVelocity = parseFloat($(this).find("radialVelocity").text());
-          let objectColor = $(this).find("objectColor").text();
-          let objectSize = parseFloat($(this).find("displayRadius").text());
-          let actualDiameter = parseFloat($(this).find("realDiameter").text());
-          let o: cObject = new cObject(cObjectNumber,name, objectColor, actualDiameter, distanceToOrigin,  radialVelocity,initialAngle);
+        $(data).find('cObject').each(function (index) {
+          const name = $(this).find('name').text();
+          const distanceToOrigin = parseFloat($(this).find('distance').text());
+          const initialAngle = parseFloat($(this).find('startAngle').text());
+          const radialVelocity = parseFloat($(this).find('radialVelocity').text());
+          const objectColor = $(this).find('objectColor').text();
+          const objectSize = parseFloat($(this).find('displayRadius').text());
+          const actualDiameter = parseFloat($(this).find('realDiameter').text());
+          const o: cObject = new cObject(cObjectNumber, name, objectColor, actualDiameter, distanceToOrigin,  radialVelocity, initialAngle);
           cObjects.push(o);
-          //console.log(o);
           cObjectNumber++;
         });
       },
       error: function () {
-        console.log("An error loading the xml file has occured");
+        console.log('An error loading the xml file has occured');
       }
     });
     return cObjects;
